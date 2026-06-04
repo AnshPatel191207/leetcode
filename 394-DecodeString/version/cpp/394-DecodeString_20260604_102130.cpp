57b190e2fd9b70cf60dfcf3793c9868948e796bd
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-04 10:21:35 +0530

    Create 394-DecodeString

---
+++ 394-DecodeString/version/cpp/394-DecodeString_20260604_102130.cpp
@@ -0,0 +1,8 @@
+// Last updated: 04/06/2026, 10:21:30
+1class Solution {
+2public:
+3    int strStr(string haystack, string needle) {
+4        int index = haystack.find(needle);
+5        return index;
+6    }
+7};
