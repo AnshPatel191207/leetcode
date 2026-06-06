3e051ea5efeb8aa521e5915a0fd7a2546131b541
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-06 19:47:06 +0530

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
@@ -0,0 +1,23 @@
+// Last updated: 06/06/2026, 19:47:00
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        
+        for (string& t : tokens) {
+            if (t == "+" || t == "-" || t == "*" || t == "/") {
+                int b = st.top(); st.pop();
+                int a = st.top(); st.pop();
+                
+                if (t == "+") st.push(a + b);
+                else if (t == "-") st.push(a - b);
+                else if (t == "*") st.push(a * b);
+                else if (t == "/") st.push(a / b);
+            } else {
+                st.push(stoi(t));
+            }
+        }
+        
+        return st.top();
+    }
+};
