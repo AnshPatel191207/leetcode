e2f519fda41666834d84471391d863e94b10c013
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-06 19:47:14 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.cpp
@@ -0,0 +1,8 @@
+// Last updated: 06/06/2026, 19:47:09
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int index = haystack.find(needle);
+        return index;
+    }
+};
