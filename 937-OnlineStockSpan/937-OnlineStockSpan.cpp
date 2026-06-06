d64646eab8acc8563b3a11dab9029e4ebfcb0322
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-06 19:46:37 +0530

    Create 937-OnlineStockSpan

---
+++ 937-OnlineStockSpan/937-OnlineStockSpan.cpp
@@ -0,0 +1,18 @@
+// Last updated: 06/06/2026, 19:46:32
+#include <stack>
+#include <utility>
+class StockSpanner {
+private:
+    std::stack<std::pair<int, int>> st;
+public:
+    StockSpanner() {}
+    int next(int price) {
+        int span = 1;
+        while (!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
