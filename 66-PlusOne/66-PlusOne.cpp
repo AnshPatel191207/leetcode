6ef2345faf8faff060d26f4f03addea0cba1470f
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-06 19:47:11 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.cpp
@@ -0,0 +1,15 @@
+// Last updated: 06/06/2026, 19:47:05
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
