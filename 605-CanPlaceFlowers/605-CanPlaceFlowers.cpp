f7cd48e8e52a39818e4c20509e5da19c12400e4e
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-07 20:06:38 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.cpp
@@ -0,0 +1,21 @@
+// Last updated: 07/06/2026, 20:06:32
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+
+        int count = 0;
+
+        for (int i = 0; i < flowerbed.size(); i++) {
+
+            if (flowerbed[i] == 0 &&
+                (i == 0 || flowerbed[i - 1] == 0) &&
+                (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+
+                flowerbed[i] = 1;
+                count++;
+            }
+        }
+
+        return count >= n;
+    }
+};
