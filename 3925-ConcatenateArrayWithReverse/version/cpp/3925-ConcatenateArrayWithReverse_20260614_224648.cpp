153b91e04aa441f5b9e277f2a700fe269add55c7
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-14 22:46:49 +0530

    Create 3925-ConcatenateArrayWithReverse

---
+++ 3925-ConcatenateArrayWithReverse/version/cpp/3925-ConcatenateArrayWithReverse_20260614_224648.cpp
@@ -0,0 +1,16 @@
+// Last updated: 14/06/2026, 22:46:48
+1class Solution {
+2public:
+3    vector<int> concatWithReverse(vector<int>& nums) {
+4        int n = nums.size();
+5
+6        vector<int> ans(2 * n);
+7
+8        for (int i = 0; i < n; i++) {
+9            ans[i] = nums[i];
+10            ans[i + n] = nums[n - i - 1];
+11        }
+12
+13        return ans;
+14    }
+15};
