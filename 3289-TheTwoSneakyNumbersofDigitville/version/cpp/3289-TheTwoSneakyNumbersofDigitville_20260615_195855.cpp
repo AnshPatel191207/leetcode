35c4c29a36a85f6038a1232e887c073e983ee9d6
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-06-15 19:58:57 +0530

    Create 3289-TheTwoSneakyNumbersofDigitville

---
+++ 3289-TheTwoSneakyNumbersofDigitville/version/cpp/3289-TheTwoSneakyNumbersofDigitville_20260615_195855.cpp
@@ -0,0 +1,16 @@
+// Last updated: 15/06/2026, 19:58:55
+1class Solution {
+2public:
+3    vector<int> getSneakyNumbers(vector<int>& nums) {
+4        sort(nums.begin(), nums.end());
+5        vector<int> result;
+6        
+7        for (size_t i = 1; i < nums.size(); ++i) {
+8            if (nums[i] == nums[i - 1]) {
+9                result.push_back(nums[i]);
+10            }
+11        }
+12        
+13        return result;
+14    }
+15};
