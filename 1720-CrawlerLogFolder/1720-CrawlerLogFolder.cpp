f37bc5d92999796228eaf8c130102b541ce1a1ae
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:16:08 +0530

    Create 1720-CrawlerLogFolder

---
+++ 1720-CrawlerLogFolder/1720-CrawlerLogFolder.cpp
@@ -0,0 +1,21 @@
+// Last updated: 05/08/2026, 11:16:06
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+
+        for (int i = 0; i < logs.size(); i++) {
+            if (logs[i] == "../") {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else if (logs[i] == "./") {
+                continue;
+            } else {
+                depth++;
+            }
+        }
+
+        return depth;
+    }
+};
