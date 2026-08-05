3ef74e3f6c4b3a669f7a4e91f870122b08ad6ed5
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:12:52 +0530

    Create 3044-MinimumOperationsToCollectElements

---
+++ 3044-MinimumOperationsToCollectElements/3044-MinimumOperationsToCollectElements.cpp
@@ -0,0 +1,22 @@
+// Last updated: 05/08/2026, 11:12:49
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_set<int> collected;
+        int operations = 0;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            operations++;
+
+            if (nums[i] <= k) {
+                collected.insert(nums[i]);
+            }
+
+            if (collected.size() == k) {
+                return operations;
+            }
+        }
+
+        return operations;
+    }
+};
