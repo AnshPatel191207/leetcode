3740e19e057e34aa659230caa6b9cbf44af6af8f
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:13:43 +0530

    Create 2566-NumberOfUnequalTripletsInArray

---
+++ 2566-NumberOfUnequalTripletsInArray/2566-NumberOfUnequalTripletsInArray.cpp
@@ -0,0 +1,26 @@
+// Last updated: 05/08/2026, 11:13:41
+class Solution {
+public:
+    int unequalTriplets(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n - 2; i++) {
+            
+            for (int j = i + 1; j < n - 1; j++) {
+                
+                for (int k = j + 1; k < n; k++) {
+                    
+                    if (nums[i] != nums[j] && 
+                        nums[i] != nums[k] && 
+                        nums[j] != nums[k]) {
+                        
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+};
