948790a762dfae3c09e89eb116c40d14002006f6
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:17:55 +0530

    Create 958-SortArrayByParityIi

---
+++ 958-SortArrayByParityIi/958-SortArrayByParityIi.cpp
@@ -0,0 +1,22 @@
+// Last updated: 05/08/2026, 11:17:53
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+        int evenIdx = 0;
+        int oddIdx = 1;
+        
+        for (int i = 0; i < n; i++) {
+            if (nums[i] % 2 == 0) {
+                result[evenIdx] = nums[i];
+                evenIdx += 2;
+            } else {
+                result[oddIdx] = nums[i];
+                oddIdx += 2;
+            }
+        }
+        
+        return result;
+    }
+};
