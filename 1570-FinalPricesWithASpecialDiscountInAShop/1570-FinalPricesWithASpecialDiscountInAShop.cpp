4e25a27064cfd1ce28adebbaf96946562f352cd6
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:16:41 +0530

    Create 1570-FinalPricesWithASpecialDiscountInAShop

---
+++ 1570-FinalPricesWithASpecialDiscountInAShop/1570-FinalPricesWithASpecialDiscountInAShop.cpp
@@ -0,0 +1,18 @@
+// Last updated: 05/08/2026, 11:16:38
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (prices[j] <= prices[i]) {
+                    prices[i] -= prices[j];
+                    break;
+                }
+            }
+        }
+        
+        return prices;
+    }
+};
