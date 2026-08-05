915e855ee6393174363adcd7230dfca67d17bf32
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:16:14 +0530

    Create 1693-SumOfAllOddLengthSubarrays

---
+++ 1693-SumOfAllOddLengthSubarrays/1693-SumOfAllOddLengthSubarrays.cpp
@@ -0,0 +1,18 @@
+// Last updated: 05/08/2026, 11:16:11
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int totalSum = 0;
+        int n = arr.size();
+
+        for (int i = 0; i < n; i++) {
+            int totalSubarrays = (i + 1) * (n - i);
+            
+            int oddSubarrays = (totalSubarrays + 1) / 2;
+            
+            totalSum += arr[i] * oddSubarrays;
+        }
+
+        return totalSum;
+    }
+};
