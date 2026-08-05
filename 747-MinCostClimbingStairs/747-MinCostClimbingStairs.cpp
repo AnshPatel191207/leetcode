83f07bd1a5d06af01758dc74e108d2b04a030f7d
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:18:34 +0530

    Create 747-MinCostClimbingStairs

---
+++ 747-MinCostClimbingStairs/747-MinCostClimbingStairs.cpp
@@ -0,0 +1,19 @@
+// Last updated: 05/08/2026, 11:18:31
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int first = cost[0];
+        int second = cost[1];
+        
+        if (n == 2) return min(first, second);
+        
+        for (int i = 2; i < n; ++i) {
+            int current = cost[i] + min(first, second);
+            first = second;
+            second = current;
+        }
+        
+        return min(first, second);
+    }
+};
