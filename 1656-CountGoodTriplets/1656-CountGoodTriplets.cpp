22a5ce40771938254afae90eac69d1f3aef1722f
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:16:17 +0530

    Create 1656-CountGoodTriplets

---
+++ 1656-CountGoodTriplets/1656-CountGoodTriplets.cpp
@@ -0,0 +1,26 @@
+// Last updated: 05/08/2026, 11:16:14
+class Solution {
+public:
+    int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+        int count = 0;
+        int n = arr.size();
+        
+        for (int i = 0; i < n - 2; i++) {
+            
+            for (int j = i + 1; j < n - 1; j++) {
+                
+                for (int k = j + 1; k < n; k++) {
+                    
+                    if (abs(arr[i] - arr[j]) <= a && 
+                        abs(arr[j] - arr[k]) <= b && 
+                        abs(arr[i] - arr[k]) <= c) {
+                        
+                        count++;
+                    }
+                }
+            }
+        }
+        
+        return count;
+    }
+};
