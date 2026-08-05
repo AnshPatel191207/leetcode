d7eb286d9889ea5da9fee3660f239e14c1043d64
Ansh Patel <ansh.h.patel.cg@gmail.com>
2026-08-05 11:21:09 +0530

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.cpp
@@ -0,0 +1,15 @@
+// Last updated: 05/08/2026, 11:21:07
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int totalProfit = 0;
+        
+        for (int i = 1; i < prices.size(); ++i) {
+            if (prices[i] > prices[i - 1]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+        
+        return totalProfit;
+    }
+};
